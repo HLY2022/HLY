8f93941141b4fb9d6e8d58450b2dcf5f1932fa32
yuhanyuan <yhy344@yhy>
2022-11-27 17:22:56 +0800

    报错

--- HLY.WEB/ClientApp/src/App.tsx
+++ HLY.WEB/ClientApp/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Route } from 'react-router';
+//import { BrowserRouter, Route } from 'react-router';
+import {BrowserRouter,Route,Link} from 'react-router-dom'
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Counter from './components/Counter';
@@ -10,11 +11,13 @@ import Usertest from './components/Usertest';
 import './custom.css'
 
 export default () => (
-    <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-        <Route path='/login' component={Login} />
-        <Route path='/usertest' component={Usertest} />
-    </Layout>
+    <BrowserRouter>
+        <Layout>
+            <Route exact path='/' component={Home} />
+            <Route path='/counter' component={Counter} />
+            <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+            <Route path='/login' component={Login} />
+            <Route path='/usertest' component={Usertest} />
+        </Layout>
+    </BrowserRouter>
 );
